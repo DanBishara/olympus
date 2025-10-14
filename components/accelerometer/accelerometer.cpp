2b022fe8b798133a291be1b3837f1e7a9f8cf576
Daniel Bishara <d2bishar@uwaterloo.ca>
2025-10-14 09:26:52 -0400

    create ImuManager singleton class

--- components/accelerometer/accelerometer.cpp
+++ components/accelerometer/accelerometer.cpp
@@ -1,8 +1,12 @@
-#include <stdint.h>
+/*
+* File: accelerometer.cpp
+* Author: Daniel Bishara
+* Date: October 13, 2025
+* Description: define class methods for the imu class
+*/
+
+#include <zephyr/drivers/sensor.h>
 
 #include "accelerometer.h"
 
-void accelerometerInit( void )
-{
-
-}
+static const struct device *imu = DEVICE_DT_GET(DT_NODELABEL(accelerometer));
