7b5c3df2ea1ed819761e07a5c1644b1274214957
Daniel Bishara <d2bishar@uwaterloo.ca>
2025-11-01 20:04:48 -0400

    enable accel irq

--- components/accelerometer/accelerometer.h
+++ components/accelerometer/accelerometer.h
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <zephyr/device.h>
+#include <lsm6dso_reg.h>
 
 #include "errorCode.h"
 
@@ -17,9 +18,11 @@ private:
     ImuManager( void ) = default;
     ~ImuManager( void ) = default;
     ErrCode_t setWakeupThreshold( uint8_t inThresholdPercent );
+    ErrCode_t enableInterrupt( void );
 public:
     static ImuManager& Instance( void ) { static ImuManager instance; return instance; }
     ErrCode_t init( void );
     ErrCode_t setFullScaleRange ( float inFullScaleRange );
     ErrCode_t setSamplingFrequency( uint16_t inSamplingFrequency );
+    ErrCode_t setPowerMode( lsm6dso_xl_hm_mode_t inPowerMode );
 };
