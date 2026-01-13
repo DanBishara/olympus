a7f5d2712eba329addc344bb4ae6d152f04955b1
Daniel Bishara <d2bishar@uwaterloo.ca>
2026-01-12 19:04:30 -0500

    add LED component for debugging LEDs

---
+++ components/LED/LED.cpp
@@ -0,0 +1,12 @@
+/*
+* File: accelerometer.cpp
+* Author: Daniel Bishara
+* Date: October 13, 2025
+* Description: define class methods for the imu class
+* Datasheet: https://www.st.com/resource/en/datasheet/lsm6dsox.pdf
+*/
+
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+
+#include "LED.h"
