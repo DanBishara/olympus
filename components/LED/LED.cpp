6a8d0fa17310e03932ef8326e6615bed44098355
Daniel Bishara <d2bishar@uwaterloo.ca>
2026-01-13 17:29:38 -0500

    add debug LED class

--- components/LED/LED.cpp
+++ components/LED/LED.cpp
@@ -6,7 +6,21 @@
 * Datasheet: https://www.st.com/resource/en/datasheet/lsm6dsox.pdf
 */
 
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/drivers/led.h>
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/logging/log.h>
 
 #include "LED.h"
+
+LOG_MODULE_REGISTER( LED, LOG_LEVEL_INF );
+
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define DEBUG_LED DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(DEBUG_LED, gpios);
