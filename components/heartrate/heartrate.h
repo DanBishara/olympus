96c379f7f8253ccd9cf01436755728a88c800d12
Daniel Bishara <d2bishar@uwaterloo.ca>
2026-03-07 00:48:29 -0500

    add thread that will sample heart rate every 2 minutes

--- components/heartrate/heartrate.h
+++ components/heartrate/heartrate.h
@@ -7,8 +7,13 @@
 
 #pragma once
 
+#include <zephyr/kernel.h>
+
 #include "errorCode.h"
 #include "ringbuffer.h"
+
+#define HEARTRATE_BUFFER_BYTES  ( 2000 ) // holds 500 float samples
+#define HEARTRATE_THREAD_STACK  ( 1024 )
 
 class HeartRateManager
 {
@@ -17,17 +22,21 @@ public:
     ErrCode_t init( float sampleRateHz );
     void      pushSample( float sample );
     ErrCode_t calculate( float *outBpm );
+    float     getLastBpm( void ) const { return lastBpm; }
 
 private:
-    static constexpr uint16_t BUFFER_BYTES = 2000; // holds 500 float samples
-    HeartRateManager( void ) : ppgBuffer( backingBuffer, BUFFER_BYTES ) {}
+    HeartRateManager( void ) : ppgBuffer( backingBuffer, HEARTRATE_BUFFER_BYTES ) {}
     ~HeartRateManager( void ) = default;
     float rollingAverage( float inNewSample );
     float calculateBaselineCurrent( float inNewSample );
+    static void threadFunc( void *p1, void *p2, void *p3 );
     bool isInit = false;
     float sampleRate = 0.0f;
+    float lastBpm = 0.0f;
     float smoothingBuffer[5] = {0};
     float baselineBuffer[100] = {0};
-    uint8_t backingBuffer[BUFFER_BYTES];
+    uint8_t backingBuffer[HEARTRATE_BUFFER_BYTES];
     RingBuffer ppgBuffer;
+    struct k_thread thread;
+    K_THREAD_STACK_MEMBER( threadStack, HEARTRATE_THREAD_STACK );
 };
